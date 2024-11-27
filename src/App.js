0b3f50c0bef36540fe2d7676d38c63111a98e173
Saif Jaffery <saifjaffery@saifs-mbp.lan>
2024-11-27 12:14:27 -0500

    intital commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="love-container">
+        <img 
+          src="love-image.jpg" 
+          alt="Love" 
+          className="love-image" 
+        />
+        <h1>I Love You Zoya</h1>
+        <div className="flowers">
+          🌸 🌹 🌺 🌷 🌼
+        </div>
+      </div>
     </div>
   );
 }
 
-export default App;
+export default App; 
